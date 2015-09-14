6706d473da5ea816624b283e32d2b54dc58e29b2
Rowan Hogan <rowan.hogan@gmail.com>
2015-09-14 17:45:28 -0400

    Fix build

--- assets/javascripts/resume-2802da68.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){return $(".contact-info").append('<span class="print-only"><a href="tel:+1-604-754-5567">+1 604 754-5567</a><span class="divider">/</span><br><a href="mailto:hello@rowanhogan.com">hello@rowanhogan.com</a></span>')})}).call(this);
